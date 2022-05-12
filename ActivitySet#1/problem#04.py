77887a1809c5265d6b66e23453f344d8b4658320
Sinchal <94883181+Sinchal@users.noreply.github.com>
2022-05-12 17:35:32 +0000

    done

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,19 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+def grade(s):
+    if(s > 0 and s < 1):
+        if(s >= 0.9):
+            print("A")
+        elif(s >= 0.8):
+            print("B")
+        elif(s >= 0.7):
+            print("C")
+        elif(s >= 0.6):
+            print("D")
+        else:
+            print("F")
+    else:
+        print("not a score")
+ 
+s = float(input("Enter the score: "))
+grade(s)
