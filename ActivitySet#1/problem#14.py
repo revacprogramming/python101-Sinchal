bb9864e8f2bf1532f9e51122f1c7fdfdf57e812f
Sinchal <94883181+Sinchal@users.noreply.github.com>
2022-07-05 10:27:59 +0000

    done

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,21 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+
+rom urllib import request
+import xml.etree.ElementTree as ET
+
+url = 'http://python-data.dr-chuck.net/https://replit.com/comments_1546784.xml'
+print ("Retrieving", url)
+html = request.urlopen(url)
+data = html.read()
+print("Retrieved",len(data),"characters")
+
+tree = ET.fromstring(data)
+results = tree.findall('comments/comment')
+icount=len(results)
+isum=0
+
+for result in results:
+    isum += float(result.find('count').text)
+print(icount)
+print(isum)
