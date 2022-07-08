4bb6b2a9b8fbbc2b3d64bc02515fb99e71abe20b
Sinchal <94883181+Sinchal@users.noreply.github.com>
2022-07-08 03:39:06 +0000

    done

--- ActivitySet#1/problem#16.py
+++ ActivitySet#1/problem#16.py
@@ -1,2 +1,26 @@
 # Databases
 # https://www.py4e.com/lessons/database
+
+import sqlite3
+conn = sqlite3.connect(':memory:')
+c = conn.cursor()
+c.execute("""CREATE TABLE Ages(
+             name VARCHAR(128),
+             age INTEGER
+             )"""
+         )
+def add_people(sName, iAge):
+    with conn:
+        c.execute("INSERT INTO Ages VALUES (:name, :age)", {'name':sName, 'age':iAge})
+
+
+lPeople = [('Emmy', 29), ('Maanisha', 24), ('Karsyn', 18), ('Elish', 39), ('Kaylan', 31)]
+for tItem in lPeople:
+    sName, iAge = tItem
+    add_people(sName, iAge)
+
+c.execute("SELECT hex(name || age) AS X FROM Ages ORDER BY X")
+print(c.fetchall())
+
+conn.commit()
+conn.close()
